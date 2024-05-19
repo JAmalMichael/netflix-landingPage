28c7f0ed3091e756e5cfa671b5090a1e58a1388f
JAmal Michael <eshimi090@gmail.com>
2024-05-19 07:21:51 -1200

    tab-2-content complete

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -40,41 +40,41 @@ const Tab = () => {
           <img 
           src="https://i.ibb.co/J2xDJV7/tab-content-1.png"
            alt="" 
-           width={500}
-           height={500} />
+           width={300}
+           height={300} />
            </div>
     </div>
     
     <div id="tab-2-content" className=' text-white mt-6'>
       <div className='grid grid-cols-1 justify-center w-[80%] mx-auto text-lg font-bold mb-6
-      sm:grid-cols-2 sm:w-[70%]'>
-        <p className='text-center'>Watch TV shows and movies anytime, anywhere - personalised for you.
+      sm:grid-cols-2 sm:w-[60%] items-center'>
+        <p className='text-center sm:text-xl'>Watch TV shows and movies anytime, anywhere - personalised for you.
         </p>
-        <button className='bg-red-700 px-5 hover:opacity-[0.9] h-7 font-medium '>
+        <button className='bg-red-700 px-5 hover:opacity-[0.9] h-7 font-medium mt-2'>
             Watch free for 30 days
         </button>
         </div>
-      <div>
-      <div  className=' items-center '>
+      <div className='grid grid-cols-1 justify-center items-center text-center sm:grid-cols-3'>
+      <div  className='flex flex-col items-center '>
       <img src="https://i.ibb.co/DpdN7Gn/tab-content-2-1.png"
-       alt="" width={500} height={500}/>
+       alt="" width={300} height={300}/>
        <p>Watch your TV</p>
-       <p className='w-[70%]'>Smart TVs, Playstation, Xbox, Chromscast,
+       <p className='w-[70%] text-gray-500'>Smart TVs, Playstation, Xbox, Chromscast,
        Apple TV, Bluu-ray
        players and more.</p>
       </div>
-      <div className='sm:w-[20%] flex flex-col items-center '>
+      <div className='flex flex-col items-center mb-4'>
       <img src="https://i.ibb.co/R3r1SPX/tab-content-2-2.png"
-       alt=""  width={500} height={500}/>
-       <p>Watch instantly or download for later</p>
-       <p>Available on phone and tablet, wherever you go.
+       alt=""  width={300} height={300} className='mb-0'/>
+       <p className='mt-0'>Watch instantly or download for later</p>
+       <p className='text-gray-500'>Available on phone and tablet, wherever you go.
        </p>
       </div>
-      <div className='sm:w-[20%] flex flex-col items-center '>
+      <div className='flex flex-col items-center '>
       <img src="https://i.ibb.co/gDhnwWn/tab-content-2-3.png" 
-      alt="" width={500} height={500}/>
+      alt="" width={300} height={300}/>
       <p>Use any computer</p>
-      <p>Watch right on Netflix.com</p>
+      <p className='text-gray-500'>Watch right on Netflix.com</p>
       </div>
       </div>
     </div>
